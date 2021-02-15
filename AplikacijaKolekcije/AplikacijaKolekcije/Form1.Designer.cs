44ed72efbfe209f19b83fbfbb90c3b41c9de42d3
UltraXDZN <ultraxdzn@gmail.com>
2021-02-15 09:17:13 +0100

    Init Commit

---
+++ AplikacijaKolekcije/AplikacijaKolekcije/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace AplikacijaKolekcije
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Podatak1Box = new System.Windows.Forms.TextBox();
+            this.Podatak1Label = new System.Windows.Forms.Label();
+            this.Podatak2Label = new System.Windows.Forms.Label();
+            this.Podatak2Box = new System.Windows.Forms.TextBox();
+            this.Podatak3Label = new System.Windows.Forms.Label();
+            this.Podatak3Box = new System.Windows.Forms.TextBox();
+            this.Podatak4Label = new System.Windows.Forms.Label();
+            this.ispisBox = new System.Windows.Forms.TextBox();
+            this.UnesiButton = new System.Windows.Forms.Button();
+            this.ObradiUnos = new System.Windows.Forms.Button();
+            this.IspisButton = new System.Windows.Forms.Button();
+            this.Podatak4Combo = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // Podatak1Box
+            // 
+            this.Podatak1Box.Location = new System.Drawing.Point(12, 26);
+            this.Podatak1Box.Name = "Podatak1Box";
+            this.Podatak1Box.Size = new System.Drawing.Size(100, 20);
+            this.Podatak1Box.TabIndex = 0;
+            // 
+            // Podatak1Label
+            // 
+            this.Podatak1Label.AutoSize = true;
+            this.Podatak1Label.Location = new System.Drawing.Point(12, 10);
+            this.Podatak1Label.Name = "Podatak1Label";
+            this.Podatak1Label.Size = new System.Drawing.Size(53, 13);
+            this.Podatak1Label.TabIndex = 1;
+            this.Podatak1Label.Text = "Podatak1";
+            // 
+            // Podatak2Label
+            // 
+            this.Podatak2Label.AutoSize = true;
+            this.Podatak2Label.Location = new System.Drawing.Point(12, 49);
+            this.Podatak2Label.Name = "Podatak2Label";
+            this.Podatak2Label.Size = new System.Drawing.Size(53, 13);
+            this.Podatak2Label.TabIndex = 3;
+            this.Podatak2Label.Text = "Podatak2";
+            // 
+            // Podatak2Box
+            // 
+            this.Podatak2Box.Location = new System.Drawing.Point(12, 65);
+            this.Podatak2Box.Name = "Podatak2Box";
+            this.Podatak2Box.Size = new System.Drawing.Size(100, 20);
+            this.Podatak2Box.TabIndex = 2;
+            // 
+            // Podatak3Label
+            // 
+            this.Podatak3Label.AutoSize = true;
+            this.Podatak3Label.Location = new System.Drawing.Point(12, 88);
+            this.Podatak3Label.Name = "Podatak3Label";
+            this.Podatak3Label.Size = new System.Drawing.Size(53, 13);
+            this.Podatak3Label.TabIndex = 5;
+            this.Podatak3Label.Text = "Podatak3";
+            // 
+            // Podatak3Box
+            // 
+            this.Podatak3Box.Location = new System.Drawing.Point(12, 104);
+            this.Podatak3Box.Name = "Podatak3Box";
+            this.Podatak3Box.Size = new System.Drawing.Size(100, 20);
+            this.Podatak3Box.TabIndex = 4;
+            // 
+            // Podatak4Label
+            // 
+            this.Podatak4Label.AutoSize = true;
+            this.Podatak4Label.Location = new System.Drawing.Point(12, 127);
+            this.Podatak4Label.Name = "Podatak4Label";
+            this.Podatak4Label.Size = new System.Drawing.Size(53, 13);
+            this.Podatak4Label.TabIndex = 7;
+            this.Podatak4Label.Text = "Podatak4";
+            // 
+            // ispisBox
+            // 
+            this.ispisBox.Location = new System.Drawing.Point(12, 170);
+            this.ispisBox.Multiline = true;
+            this.ispisBox.Name = "ispisBox";
+            this.ispisBox.Size = new System.Drawing.Size(610, 268);
+            this.ispisBox.TabIndex = 8;
+            // 
+            // UnesiButton
+            // 
+            this.UnesiButton.BackColor = System.Drawing.Color.DarkRed;
+            this.UnesiButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.UnesiButton.Location = new System.Drawing.Point(145, 22);
+            this.UnesiButton.Name = "UnesiButton";
+            this.UnesiButton.Size = new System.Drawing.Size(162, 140);
+            this.UnesiButton.TabIndex = 9;
+            this.UnesiButton.Text = "Unesi";
+            this.UnesiButton.UseVisualStyleBackColor = false;
+            this.UnesiButton.Click += new System.EventHandler(this.UnesiButton_Click);
+            // 
+            // ObradiUnos
+            // 
+            this.ObradiUnos.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ObradiUnos.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.ObradiUnos.Location = new System.Drawing.Point(313, 24);
+            this.ObradiUnos.Name = "ObradiUnos";
+            this.ObradiUnos.Size = new System.Drawing.Size(149, 140);
+            this.ObradiUnos.TabIndex = 10;
+            this.ObradiUnos.Text = "Obradi";
+            this.ObradiUnos.UseVisualStyleBackColor = false;
+            this.ObradiUnos.Click += new System.EventHandler(this.ObradiUnos_Click);
+            // 
+            // IspisButton
+            // 
+            this.IspisButton.BackColor = System.Drawing.Color.Orange;
+            this.IspisButton.Location = new System.Drawing.Point(468, 22);
+            this.IspisButton.Name = "IspisButton";
+            this.IspisButton.Size = new System.Drawing.Size(154, 141);
+            this.IspisButton.TabIndex = 11;
+            this.IspisButton.Text = "Ispis";
+            this.IspisButton.UseVisualStyleBackColor = false;
+            this.IspisButton.Click += new System.EventHandler(this.IspisButton_Click);
+            // 
+            // Podatak4Combo
+            // 
+            this.Podatak4Combo.FormattingEnabled = true;
+            this.Podatak4Combo.Items.AddRange(new object[] {
+            "M",
+            "Ž"});
+            this.Podatak4Combo.Location = new System.Drawing.Point(12, 143);
+            this.Podatak4Combo.Name = "Podatak4Combo";
+            this.Podatak4Combo.Size = new System.Drawing.Size(100, 21);
+            this.Podatak4Combo.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 450);
+            this.Controls.Add(this.Podatak4Combo);
+            this.Controls.Add(this.IspisButton);
+            this.Controls.Add(this.ObradiUnos);
+            this.Controls.Add(this.UnesiButton);
+            this.Controls.Add(this.ispisBox);
+            this.Controls.Add(this.Podatak4Label);
+            this.Controls.Add(this.Podatak3Label);
+            this.Controls.Add(this.Podatak3Box);
+            this.Controls.Add(this.Podatak2Label);
+            this.Controls.Add(this.Podatak2Box);
+            this.Controls.Add(this.Podatak1Label);
+            this.Controls.Add(this.Podatak1Box);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Podatak1Box;
+        private System.Windows.Forms.Label Podatak1Label;
+        private System.Windows.Forms.Label Podatak2Label;
+        private System.Windows.Forms.TextBox Podatak2Box;
+        private System.Windows.Forms.Label Podatak3Label;
+        private System.Windows.Forms.TextBox Podatak3Box;
+        private System.Windows.Forms.Label Podatak4Label;
+        private System.Windows.Forms.TextBox ispisBox;
+        private System.Windows.Forms.Button UnesiButton;
+        private System.Windows.Forms.Button ObradiUnos;
+        private System.Windows.Forms.Button IspisButton;
+        private System.Windows.Forms.ComboBox Podatak4Combo;
+    }
+}
+
---
+++ AplikacijaKolekcije/AplikacijaKolekcije/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplikacijaKolekcije
+{
+    public partial class Form1 : Form
+    {
+        List<List<string>> datas = new List<List<string>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UnesiButton_Click(object sender, EventArgs e)
+        {
+            datas.Add(new List<string>() { Podatak1Box.Text, Podatak2Box.Text, Podatak3Box.Text, Podatak4Combo.Text });
+            Podatak1Box.Text = "";
+            Podatak2Box.Text = "";
+            Podatak3Box.Text = "";
+            Podatak4Combo.Text = "";
+        }
+
+        private void IspisButton_Click(object sender, EventArgs e)
+        {
+            ispisBox.Text = "Podatak1\t\tPodatak2\t\tPodatak3\t\tPodatak4\t\tDodatak\r\n";
+            foreach (var data in datas)
+            {
+                foreach (string text in data)
+                {
+                    ispisBox.Text += $"{text}\t\t";
+                }
+                ispisBox.Text += "\r\n";
+            }
+        }
+
+        private void ObradiUnos_Click(object sender, EventArgs e)
+        {
+            foreach (var data in datas)
+            {
+                if (data.Count == 4)
+                {
+                    if (data[3] == "M")
+                    {
+                        data.Add("Ima Brkove");
+                    }
+                    else data.Add("Nema Brkove");
+                }
+            }
+        }
+    }
+}
